9d8997efbd514f822d6eaec73bf67aec9befedd3
IvanRomero-io <ivandejesusrl2001@gmail.com>
2023-01-29 00:56:31 -0500

    Renderizando los objetos al canvas

--- Juego-js/game.js
+++ Juego-js/game.js
@@ -1,11 +1,17 @@
 const canvas = document.querySelector("#game")
 const game = canvas.getContext('2d')
+let canvasSize //Cree una variable que un if le dara un valor, esta variable es para el tamaño del canvas
+let elementsSize
 
-window.addEventListener('load', startGame)
 
-function startGame(){
-    let canvasSize //Cree una variable que un if le dara un valor, esta variable es para el tamaño del canvas
 
+window.addEventListener('load', setCanvasSize)
+window.addEventListener('resize', setCanvasSize)
+
+
+
+function setCanvasSize(){
+   
     if(window.innerHeight > window.innerWidth){
         canvasSize = window.innerWidth * 0.8
 
@@ -19,24 +25,45 @@ function startGame(){
     canvas.setAttribute('width', canvasSize)
     canvas.setAttribute('height', canvasSize)
 
-    const elementsSize = canvasSize / 10
+    elementsSize = canvasSize / 10
 
-    game.font = (elementsSize - 12) + "px Verdana"  //Con este metodo se cambia el tamaño de fuente y familia
+ console.log({canvasSize, elementsSize})
+
+
+    startGame()
+}
+
+
+function startGame(){
+
+    game.font = (elementsSize - 15) + "px Verdana"  //Con este metodo se cambia el tamaño de fuente y familia
     game.fillStyle ="orange" //Con este por ahora se que cambia el color
     game.textAlign = "end" //Este metodo es para que lon que ingreses empiece o termine justamente donde le mandas la ubicacion en el fill, por ejemplo, si pones una ubicacion 25, 25... no va a centrarce en esa ubicacion, va es a empezar justo alli, o terminar hasta alli.o otras como "center" que esa si lo centra en un punto justo de las hubicaciones
     // game.fillText("Prueba", 0, 25)
 
-    console.log({canvasSize, elementsSize})
+    const mapRows = maps[0].trim().split("\n")
 
-    for (let i = 1; i < 11 ; i++) {
-        game.fillText(emojis['X'], elementsSize * i , elementsSize )
-        
+    const mapRowsSinEspacios = mapRows.map((row) => row.trim().split(''))
+
+    console.log(mapRows, mapRowsSinEspacios )
+
+
+for(let y = 1;  y < 11 ; y++){ 
+    // console.log({y})
+    for (let x = 1; x < 11 ; x++) {
+        game.fillText(emojis[mapRowsSinEspacios[y - 1][x - 1]], elementsSize * x , elementsSize * y - 10)
+        // console.log({elementsSize, x})
     }
-
+}
     //  game.fillRect(0, 5, 100, 100)
     //  game.clearRect(0, 0, 50, 50)
     //  game.clearRect(50, 50, 100, 100)
    
     
-    
+
 }
+
+
+//NUEVO METODO ".split()", este metodo nos sirve para convertir en elemento de array a cada letra de un string, o tambien si que remos que solo sean las paralabra, le podemos añadir un espacio para que sepa desde donde va a ser el inicio y el final de cada elemento que va crear, por ejemplo .split(" ")
+
+//NUEVO METODO ".trim()" elimina los espacios vacios de cada string
